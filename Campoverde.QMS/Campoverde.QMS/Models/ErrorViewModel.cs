931aac55ad94603c656ad252dfce57e56087b37e
Sifat Hossain <sifat.hossain@microsec.co.uk>
2024-05-14 00:24:21 +0600

    Initial commit

---
+++ Campoverde.QMS/Campoverde.QMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Campoverde.QMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
