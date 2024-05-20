deee9bc35636f1cc040957237cd987be60f7ee24
Sifat Hossain <sifat1258@gmail.com>
2024-05-20 23:21:33 +0600

    init

---
+++ Campoverde.QMS/Campoverde.QMS/ViewModel/QuoteViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Campoverde.QMS.Enums;
+
+namespace Campoverde.QMS.ViewModel;
+
+public class QuoteViewModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string SpanishAddress { get; set; }
+    public int VehicleId { get; set; }
+    public VehicleSizeEnum VehicleSize { get; set; }
+    public VehicleTypeEnum VehicleType { get; set; }
+}
