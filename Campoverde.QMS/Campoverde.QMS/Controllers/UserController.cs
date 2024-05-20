99f9ff839e4974e35160d6610a26310b14c67115
Sifat Hossain <sifat1258@gmail.com>
2024-05-20 09:15:49 +0600

    remove angular js file, cretae controllers

---
+++ Campoverde.QMS/Campoverde.QMS/Controllers/UserController.cs
@@ -0,0 +1,152 @@
+﻿using Campoverde.QMS.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Campoverde.QMS.Controllers
+{
+    public class UserController(CampoverdeDbContext context) : Controller
+    {
+        private readonly CampoverdeDbContext _context = context;
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+            var campoverdeDbContext = _context.User.Include(u => u.Role);
+            return View(await campoverdeDbContext.ToListAsync());
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            ViewData["RoleId"] = new SelectList(_context.Role, "Id", "Name");
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Email,Phone,Password,RoleId,Id,IsDeleted,IsActive")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Role, "Id", "Id", user.RoleId);
+            return View(user);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoleId"] = new SelectList(_context.Role, "Id", "Id", user.RoleId);
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Email,Phone,Password,RoleId,Id,IsDeleted,IsActive")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Role, "Id", "Id", user.RoleId);
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+            if (user != null)
+            {
+                _context.User.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.User.Any(e => e.Id == id);
+        }
+    }
+}
--- Campoverde.QMS/Campoverde.QMS/Models/User.cs
+++ Campoverde.QMS/Campoverde.QMS/Models/User.cs
@@ -1,9 +1,13 @@
-﻿namespace Campoverde.QMS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Campoverde.QMS.Models
 {
     public class User : BaseEntity
     {
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public int RoleId { get; set; }
         public Role Role { get; set; }
