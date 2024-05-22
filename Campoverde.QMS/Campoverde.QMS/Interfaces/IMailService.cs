26f255fe8e4089f1fcdb0b0f25b9bed1dae62261
Sifat Hossain <sifat1258@gmail.com>
2024-05-22 19:44:31 +0600

    modify email service

--- Campoverde.QMS/Campoverde.QMS/Interfaces/IMailService.cs
+++ Campoverde.QMS/Campoverde.QMS/Interfaces/IMailService.cs
@@ -3,5 +3,5 @@
 public interface IMailService
 {
     public void SendMailToMany(string[] toaddress, string[] ccaddress, string[] bccaddress, string subject, string body);
-    public void SendMail(string toaddress, string ccaddress, string subject, string body);
+    public void SendMail(string toaddress, string ccaddress, string subject, string name);
 }
